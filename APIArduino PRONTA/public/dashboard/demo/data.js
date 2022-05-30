8a06d28c545a6d46ae8f2cde250f8ff233db95e7
Lykked <lucas.osilva@sptech.school>
2022-05-30 00:19:13 -0300

    DashCompleta

---
+++ APIArduino PRONTA/public/dashboard/demo/data.js
@@ -0,0 +1,85 @@
+var data2 = [];
+
+
+//segundo//
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 200 + i / 1.5 + 00, 
+        Math.random() * 40 - i / 25 + 220, Math.random()]);
+}
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 200 + i / 1.5 + 00, 
+        Math.random() * 40 - i / 25 + 220, Math.random()]);
+}
+// //primeiro//
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 200 + i / 1.5 + 00, 
+        Math.random() * 25 - i / 25 + 130, Math.random()]);
+}
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 200 + i / 1.5 + 00, 
+        Math.random() * 25 - i / 25 + 130, Math.random()]);
+}
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 200 + i / 1.5 + 00, 
+        Math.random() * 25 - i / 25 + 130, Math.random()]);
+}
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 200 + i / 1.5 + 00, 
+        Math.random() * 25 - i / 25 + 130, Math.random()]);
+}
+
+
+// //teceriro//
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 120 + i / 1.5 + 50, 
+        Math.random() * 30 - i / 25 + 330, Math.random()]);
+}
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 120 + i / 1.5 + 50, 
+        Math.random() * 30 - i / 25 + 330, Math.random()]);
+}for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 120 + i / 1.5 + 50, 
+        Math.random() * 30 - i / 25 + 330, Math.random()]);
+}
+
+
+//quarto//
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 120 + i / 1.5 + 50, 
+        Math.random() * 30 - i / 25 + 420, Math.random()]);
+}
+
+// //canto//
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 50 + i / 1.7 + 350, 
+        Math.random() * 250 - i / 25 + 72, Math.random()]);
+}
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 50 + i / 1.7 + 350, 
+        Math.random() * 250 - i / 25 + 72, Math.random()]);
+}
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 50 + i / 1.7 + 350, 
+        Math.random() * 250 - i / 25 + 112, Math.random()]);
+}
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 50 + i / 1.7 + 350, 
+        Math.random() * 250 - i / 25 + 72, Math.random()]);
+}
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 50 + i / 1.7 + 350, 
+        Math.random() * 250 - i / 25 + 72, Math.random()]);
+}
+
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 10 + i / 1.7 + 400, 
+        Math.random() * 100 - i / 25 +350, Math.random()]);
+}
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 10 + i / 1.7 + 400, 
+        Math.random() * 100 - i / 25 +350, Math.random()]);
+}
+for (var i = 0; i < 500; ++i) {
+    data2.push([Math.random() * 10 + i / 1.7 + 400, 
+        Math.random() * 100 - i / 25 +350, Math.random()]);
+}
